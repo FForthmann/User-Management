66e3a85309eebbdb2a5775cb780e291c88633a5f
Luca Ulrich - A18 <l.ulrich@nordakademie.de>
2021-11-04 19:24:06 +0100

    Implements Calculation for leavingDate
    Luca Ulrich - 60 min

--- Frontend-Angular/src/app/components/user-form/user-form.component.ts
+++ Frontend-Angular/src/app/components/user-form/user-form.component.ts
@@ -37,7 +37,11 @@ export class UserFormComponent {
    * @returns: User Object
    */
   private buildUser(userData: formUser): User {
-    return {
+    //Austritte erfolgen immer zum Jahresende, sie müssen mindestens 3 Monate vor Ende des
+    // Kalenderjahres eingereicht werden
+    // 30-09 letztes Datum des Jahres
+    let leavingDate: Date = new Date();
+    let userObject: User = {
       name: {
         firstName: userData.firstName,
         lastName: userData.lastName
@@ -51,10 +55,41 @@ export class UserFormComponent {
       birthday: (this.datePipe.transform(userData.birthday, 'yyyy-MM-dd') as string),
       entryDate: (this.datePipe.transform(userData.entryDate, 'yyyy-MM-dd') as string),
       cancellationDate: "" ? "": (this.datePipe.transform(userData.cancellationDate, 'yyyy-MM-dd') as string),
-      leavingDate: "" ? "": (this.datePipe.transform(userData.leavingDate, 'yyyy-MM-dd') as string),
       memberType: userData.memberType,
       accountDetails: userData.accountDetails,
       familyId: userData.familyId? parseInt(userData.familyId): undefined
     }
+
+    if (userData.cancellationDate) {
+      leavingDate = this.calculateLeavingDate(userData.cancellationDate);
+      userObject['leavingDate'] = this.datePipe.transform(leavingDate, 'yyyy-MM-dd') as string;
+    }
+
+    return userObject;
+  }
+
+  /**
+   * Helper-Function to calculate the leaving Date of a Member.
+   * If a User cancels his Membership after the 09-30, he will be a member for another year.
+   * Memberships are always active until the last Day of a year (12-31)
+   * Ex: cancel(2020-10-01) -> leaving (12-31-2021)
+   *
+   * @Author: Luca Ulrich
+   * @param cancellationDate: Date - Date on which a User cancels his Membership
+   * @private
+   * @returns: date
+   */
+  private calculateLeavingDate(cancellationDate: string): Date {
+    let leavingDate: Date;
+    const cancellationDateObject = new Date(cancellationDate);
+    const year = cancellationDateObject.getFullYear();
+    const month = cancellationDateObject.getMonth();
+
+    if (month > 8) {
+      leavingDate = new Date(year + 1, 11, 31);
+    } else {
+      leavingDate = new Date(year, 11, 31);
+    }
+    return leavingDate;
   }
 }
