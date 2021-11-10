6cef7088a18bd8f971a1a2835f594738dc125d94
Ridvan Cetin <ridvan.cetin@nordakademie.de>
2021-11-10 18:46:10 +0100

    WIP: MemberType Sql added
    Fabian & Ridvan

---
+++ Backend-Spring/src/main/resources/insertMemberTypes.sql
@@ -0,0 +1,5 @@
+INSERT INTO MEMBER_TYPE (DESCRIPTION, AMOUNT) VALUES ( 'Vollmitglied', 25.0 ),
+                                                     ( 'Ermäßigt', 23.0 ),
+                                                     ( 'Jugendlich', 15.0 ),
+                                                     ( 'Fördermitglied', 10.0 ),
+                                                     ( 'Rabatt für Familienmitglieder', 15.0 );
