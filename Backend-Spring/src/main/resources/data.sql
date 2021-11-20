82ca0acaa4923bbf8dc28e0c98f8065c063f332c
Ridvan Cetin <ridvan.cetin@nordakademie.de>
2021-11-20 15:38:29 +0100

    WIP:
    Ridvan

--- Backend-Spring/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO MEMBER_TYPE (DESCRIPTION, AMOUNT) VALUES ( 'Vollmitglied', 25.0 ),
-                                                     ( 'Ermäßigt', 23.0 ),
-                                                     ( 'Jugendlich', 15.0 ),
-                                                     ( 'Fördermitglied', 10.0 ),
-                                                     ( 'Rabatt für Familienmitglieder', 15.0 );
