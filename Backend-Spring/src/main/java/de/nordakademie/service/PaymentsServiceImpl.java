e3f34bf62e0cb939f84ace91b18e9113879503b0
Fabian Forthmann <fabian.forthmann@nordakademie.de>
2021-11-21 20:01:47 +0100

    Computing date of leaving
    Fabian 2 Stunden

--- Backend-Spring/src/main/java/de/nordakademie/service/PaymentsServiceImpl.java
+++ Backend-Spring/src/main/java/de/nordakademie/service/PaymentsServiceImpl.java
@@ -1,6 +1,5 @@
 package de.nordakademie.service;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import javax.inject.Inject;
@@ -47,50 +46,16 @@ public class PaymentsServiceImpl implements PaymentsService {
     @Override
     public Payments createPayments(Payments payments) {
 
-       if (checkBankAccountDetails(payments)){
+        if (checkBankAccountDetails(payments)) {
             throw new IllegalArgumentException("Luca ist nen Noob");
-       }
+        }
 
         // Check if User exists in DB
-       if (payments.getUserId() == null || !existsUserInDB(payments)) {
-          throw new IllegalArgumentException(ApiMessages.MSG_USER_NOT_IN_DB);
+        if (payments.getUserId() == null || !existsUserInDB(payments)) {
+            throw new IllegalArgumentException(ApiMessages.MSG_USER_NOT_IN_DB);
         }
 
         return repository.save(payments);
-    }
-
-    private boolean checkBankAccountDetails(Payments payments) {
-
-        String bankAccountDetails = payments.getBankAccountDetails();
-
-        String[] array = bankAccountDetails.split("");
-        if(array.length!= 22){
-            return true;
-        }
-        if(!array[0].chars().allMatch(Character::isLetter) && !array[1].chars().allMatch(Character::isLetter)){
-            return true;
-        } else {
-            for (int i = 2; i< array.length;i++){
-                if(!array[i].matches("[0-9]")){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    /**
-     * Check if User exists in DB
-     *
-     * @param payments Payments Object
-     * @return Boolean-Value if User exists in DB
-     */
-    private boolean existsUserInDB(Payments payments) {
-        return this.userService
-                .findUserById(payments
-                                      .getUserId()
-                                      .getUserId())
-                .isPresent();
     }
 
     /**
@@ -113,7 +78,7 @@ public class PaymentsServiceImpl implements PaymentsService {
     @Override
     public void updatePayments(Long id, Payments payments) {
 
-        if (checkBankAccountDetails(payments)){
+        if (checkBankAccountDetails(payments)) {
             throw new IllegalArgumentException("Luca ist nen Noob");
         }
 
@@ -169,6 +134,44 @@ public class PaymentsServiceImpl implements PaymentsService {
     @Override
     public boolean existsUserInPayments(long userId) {
         return repository.existsUserInPayments(userId);
+    }
+
+    private boolean checkBankAccountDetails(Payments payments) {
+
+        String bankAccountDetails = payments.getBankAccountDetails();
+
+        String[] array = bankAccountDetails.split("");
+        if (array.length != 22) {
+            return true;
+        }
+        if (!array[0]
+                .chars()
+                .allMatch(Character::isLetter) && !array[1]
+                .chars()
+                .allMatch(Character::isLetter)) {
+            return true;
+        } else {
+            for ( int i = 2; i < array.length; i++ ) {
+                if (!array[i].matches("[0-9]")) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Check if User exists in DB
+     *
+     * @param payments Payments Object
+     * @return Boolean-Value if User exists in DB
+     */
+    private boolean existsUserInDB(Payments payments) {
+        return this.userService
+                .findUserById(payments
+                                      .getUserId()
+                                      .getUserId())
+                .isPresent();
     }
 
 }
